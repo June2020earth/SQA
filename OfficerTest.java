5367d605ef02b94223097055bbd80b96c2ba7786
June2020earth <75258378+June2020earth@users.noreply.github.com>
2022-06-26 14:42:36 +0800

    Test Cases

---
+++ OfficerTest.java
@@ -0,0 +1,25 @@
+import static org.junit.Assert.*;
+import org.junit.Test;
+public class OfficerTest {
+    @Test
+    public void testGetLeftEdge(){
+        ShopOwner rectangle = new ShopOwner(10.00, 15.00,10.00,3.00);
+        assertEquals(10.00,rectangle.getLeftEdge());
+    }
+    @Test
+    public void testGetRightEdge(){
+        Rectangle rectangle = new Rectangle(10.00, 15.00,10.00,3.00);
+        assertEquals(10.00,rectangle.getRightEdge());
+    }
+    @Test
+    public void testGetTopEdge(){
+        Rectangle rectangle = new Rectangle(10.00, 15.00,10.00,3.00);
+        assertEquals(15.00,rectangle.getTopEdge());
+    }
+    @Test
+    public void testGetBottomEdge(){
+        Rectangle rectangle = new Rectangle(10.00, 15.00,10.00,3.00);
+        assertEquals(3.00,rectangle.getLeftEdge(),0.02);
+    }
+}
+
